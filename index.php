583b266ffa1390e4044d2eb776bb144a6e3008c4
saurabh2059 <aryalsaurav141@gmail.com>
2024-08-22 19:16:56 +0545

    calculator project

--- index.php
+++ index.php
@@ -3,54 +3,85 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>calc</title>
 </head>
-<body style="background: grey;">
+<body>
     
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
 
-<!-- <form action="/formhandler.php" method="post">
-    <label for="name">Your name:</label>
-    <input name="name" id="name" type="text">
+<input type="number" name="num1" placeholder="number 1 ">
+<select name="operator">
+    <option value="add">+</option>
+    <option value="min">-</option>
+    <option value="mul">*</option>
+    <option value="div">/</option>
+    <option value="mod">%</option>
+</select>
+<input type="number" name="num2" placeholder="number 2 ">
 
-    <label for="age">Your age:</label>
-    <input name="age" id="age" type="number">
 
-    <label for="favouritepet">favouritepet</label>
-    <select name="favouritepet" id="favouritepet">
-        <option value="none">NONE</option>
-        <option value="dog">DOG</option>
-        <option value="cat">CAT</option>
-        <option value="parrot">PARROT</option>
-    </select>
 
-    <button type="submit">Submit</button>
-</form> -->
 
+<button type="submit"> CALCULATE</button>
+
+
+</form>
+   
 <?php
+     if( $_SERVER["REQUEST_METHOD"] == "POST" ){
+        // grab data
+        $num1 = filter_input(INPUT_POST, "num1", FILTER_SANITIZE_NUMBER_FLOAT);
+        $num2 = filter_input(INPUT_POST, "num2", FILTER_SANITIZE_NUMBER_FLOAT);
+        $operator = htmlspecialchars($_POST['operator']);
 
-$a = "saurabh";
-$b = "aryal";
+      
+      
+        //error handler
 
-$c = $a ." ".$b;
-echo $c;
+        $error= false;
+        if( empty($num1)|| empty($num2) || empty($operator) ){
+            echo " filll all input";
+            $error = true;
+        }
+        if(!is_numeric($num1) || !is_numeric($num2)){
+            echo "enter number value";
+            $error=true;
+        }
+        // calculate the number if no error
 
+        if(!$error){
+            $value = 0;
+                switch($operator){
 
-// arithmentic operator
-echo"<br>";
-$a = 1; $b = 1;
-$c = $a+$b;
-echo $c;
-echo 10**3; //power
+                    case "add":
+                      $value =   $num1+$num2;
+                      break;
+                    case "min":
+                      $value =   $num1-$num2;
+                      break;
+                    case "mul":
+                      $value =   $num1*$num2;
+                      break;
+                    case "div":
+                      $value =   $num1/$num2;
+                      break;
+                    case "mod":
+                      $value =   $num1%$num2;
+                      break;
 
-//logical   ----->  and , or , || , &&
-$c = 5;
-$d= 5;
+                      default:
+                      echo "not identified operator";
+                }
 
-if( $a == $b && $c==$d || $a == $d){
-    echo "wow";
-}
+        }
+
+        echo "result is " . $value ;
+     }
 
 ?>
+
+
+
 
 
 </body>
---
+++ operators.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background: grey;">
+    
+
+<!-- <form action="/formhandler.php" method="post">
+    <label for="name">Your name:</label>
+    <input name="name" id="name" type="text">
+
+    <label for="age">Your age:</label>
+    <input name="age" id="age" type="number">
+
+    <label for="favouritepet">favouritepet</label>
+    <select name="favouritepet" id="favouritepet">
+        <option value="none">NONE</option>
+        <option value="dog">DOG</option>
+        <option value="cat">CAT</option>
+        <option value="parrot">PARROT</option>
+    </select>
+
+    <button type="submit">Submit</button>
+</form> -->
+
+<?php
+
+$a = "saurabh";
+$b = "aryal";
+
+$c = $a ." ".$b;
+echo $c;
+
+
+// arithmentic operator
+echo"<br>";
+$a = 1; $b = 1;
+$c = $a+$b;
+echo $c;
+echo 10**3; //power
+
+//logical   ----->  and , or , || , &&
+$c = 5;
+$d= 5;
+
+if( $a == $b && $c==$d || $a == $d){
+    echo "wow";
+}
+
+$result = match ($c) {
+     1,3,4,5 =>"var a is equal to 1" ,
+     2 => "var a is equal to 2",
+     default => "none",
+};
+
+echo $result;
+
+?>
+
+
+</body>
+</html>
